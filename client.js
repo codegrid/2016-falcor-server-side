e201bbdae5882c9666bfa8ed08f49756368e6d23
ykhs <yukihisa.yamada@gmail.com>
2016-06-21 03:59:38 +0900

    クライアント側での動作確認コード

--- client.js
+++ client.js
@@ -1,1 +1,7 @@
-console.log('It works!');
+const model = new falcor.Model({
+  source: new falcor.HttpDataSource('/model.json')
+});
+
+model.get('hello').then(res => {
+  console.log(JSON.stringify(res, null, 2));
+});
