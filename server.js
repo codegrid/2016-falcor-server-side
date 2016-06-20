7270702e787e61f7e3f34df577ec4d6db6ce1881
ykhs <yukihisa.yamada@gmail.com>
2016-06-21 03:56:54 +0900

    falcor-expressのセットアップ

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const falcorExpress = require('falcor-express');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/model.json', falcorExpress.dataSourceRoute((req, res) => {
+  // TODO: FalcorRouterインスタンスを返す
+}));
 app.use(express.static('./'));
 
 const server = app.listen((process.env.PORT || 3000), (err) => {
